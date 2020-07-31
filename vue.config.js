2d75303f5bac8bfde3abbb80210eb1d7635ca6fd
kscript <1258@live.cn>
2020-07-31 11:40:35 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,22 @@
+const path = require('path')
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
 module.exports = {
     productionSourceMap: false,
     pages: {
         index: {
-            entry: "examples/main.js",
-            template: "public/index.html",
-            filename: "index.html"
+            entry: 'examples/main.js',
+            template: 'public/index.html',
+            filename: 'index.html'
         }
     },
     css: {
         extract: false
     },
     chainWebpack: config => {
+        config.resolve.alias
+            .set('@@', resolve('examples'))
         config.module
             .rule('js')
             .include
