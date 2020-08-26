dae25a1648d1dc9ae2b27e045d7b454a6d997a89
kscript <1258@live.cn>
2020-08-26 11:23:21 +0800

    update

--- examples/views/Home.vue
+++ examples/views/Home.vue
@@ -224,13 +224,6 @@
         UP
       </div>
     </el-backtop>
-    <button @click="show = !show">
-    Toggle
-  </button>
-  
-  <transition name="fade">
-    <p v-if="show">hello</p>
-  </transition>
   </div>
 </template>
 <script lang="ts">
@@ -273,10 +266,8 @@ export default defineComponent({
       { name: '标签四', type: 'warning' },
       { name: '标签五', type: 'danger' }
     ])
-    const show = ref(true)
     return {
       ...state,
-      show,
       itemName,
       alerts,
       tags
