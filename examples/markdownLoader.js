f24d327cc0b602ab1a8f8dacc7e49b0f6311b25a
kscript <1258@live.cn>
2020-08-31 12:55:31 +0800

    修复生产环境下md注释被vue-loader过滤

--- examples/markdownLoader.js
+++ examples/markdownLoader.js
@@ -1,11 +1,44 @@
 const markdown = require('markdown-it')
 
+const transform = function(src) {
+  const start = '<!--'
+  const end = '-->'
+  const regS = new RegExp(start, 'g')
+  const regE = new RegExp(end, 'g')
+  let curr
+  try {
+    while(curr = regS.exec(src)) {
+      const startIndex = curr.index + start.length
+      const text = src.slice(startIndex)
+      const next = regE.exec(text)
+      if (next) {
+        src = src.slice(0, curr.index)
+        + '{{`'
+        + src.slice(startIndex, startIndex + next.index).replace(/(\{\{|\}\}|`)/g, function (s) {
+          return {
+            '{{' : '\\{\\{',
+            '}}' : '\\}\\}',
+            '`': '\\`'
+          }[s]
+        })
+        + '`}}'
+        + src.slice(startIndex + next.index + end.length)
+        regS.lastIndex = regE.lastIndex = 0
+      } else {
+        break
+      }
+    }
+  } catch (err) {
+    console.log(err)
+  }
+  return src
+}
 module.exports = function(src) {
   const html = markdown({
     html: true,
     linkify: true,
     typographer: true
-  }).render(src)
+  }).render(transform(src))
   return (
     `<template>\n` +
       `<div class="markdown">${html}</div>\n` +
