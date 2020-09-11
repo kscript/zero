e44cb3d8781a908354b92020ac34b31576c8acf8
kscript <1258@live.cn>
2020-09-11 15:04:36 +0800

    添加perttier配置

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+module.exports = { 
+  // 末尾分号
+  "semi": false,
+  // 单引号
+  "singleQuote": true,
+  "useTabs": false,
+  "tabWidth": 2,
+  // 行尾逗号 none | es5 | all
+  "trailingComma": "none",
+  // 箭头函数参数括号 avoid | always
+  "arrowParens": "avoid",
+  // 对象中的空格
+  "bracketSpacing": true,
+  // 超出是否换行
+  "proseWrap": "preserve"
+}
