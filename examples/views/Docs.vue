56f846a5f786c38809c7d71ea5557568f7615b98
kscript <1258@live.cn>
2020-09-21 11:34:28 +0800

    添加底部切换链接

--- examples/views/Docs.vue
+++ examples/views/Docs.vue
@@ -4,7 +4,7 @@
       <el-aside width="240px">
         <el-scrollbar class="full-screen-height">
           <ul class="componet-list">
-            <li v-for="vo in componets" :key="vo.name">
+            <li v-for="vo in components" :key="vo.name">
               <router-link :to="{ path: '/docs/' + vo.name }">
                 <el-link :type="vo.name === activeName ? 'primary' : 'default'" @click="clickHandler(vo.name)">
                   {{vo.title}}
@@ -14,30 +14,69 @@
           </ul>
         </el-scrollbar>
       </el-aside>
-      <el-main>
-        <router-view />
-      </el-main>
-      <!-- <el-aside></el-aside> -->
+      <el-container>
+        <el-main>
+          <router-view />
+        </el-main>
+        <!-- <el-aside></el-aside> -->
+        <el-footer height="80px">
+          <router-link v-if="prev.name" :to="{
+            path: '/docs/' + prev.name
+          }">
+            <el-link :underline="false">
+              <el-icon name="arrow-left"></el-icon>
+              {{prev.title || ''}}
+            </el-link>
+          </router-link>
+          <router-link v-if="next.name" :to="{
+            path: '/docs/' + next.name
+          }">
+            <el-link class="right" :underline="false">
+              {{next.title || ''}}
+              <el-icon name="arrow-right"></el-icon>
+            </el-link>
+          </router-link>
+        </el-footer>
+      </el-container>
     </el-container>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { computed, defineComponent, ref, watchEffect } from 'vue'
 import { list } from './Components'
 import { useRoute } from 'vue-router'
+import { components } from 'packages'
+
 export default defineComponent({
   name: 'Home',
   setup() {
     const route = useRoute()
-    const componets = ref(list)
+    const components = ref(list)
+    const activeIndex = ref(-1)
     const activeName = ref(route.params.id)
     const clickHandler = (name: string) => {
       activeName.value = name
     }
+    const prev = computed(() => {
+      const index = activeIndex.value - 1
+      return index < 0 ? {} : components.value[index]
+    })
+    const next = computed(() => {
+      const index = activeIndex.value + 1
+      return index < 0 ? {} : components.value[index]
+    })
+
+    watchEffect(() => {
+      activeIndex.value = components.value.findIndex(item => item.name === route.params.id)
+    })
+
+    console.log(prev, next, activeIndex)
     return {
       activeName,
       clickHandler,
-      componets
+      components,
+      prev,
+      next
     }
   }
 })
@@ -59,6 +98,13 @@ export default defineComponent({
   li {
     list-style: none;
     padding: 10px;
+  }
+}
+.el-footer {
+  padding: 15px;
+  box-sizing: border-box;
+  .right {
+    float: right;
   }
 }
 </style>
